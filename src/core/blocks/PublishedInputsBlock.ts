30d23143ab8afe3719f19ad750ad71af8214804d
Trevor Behlman <trevor.behlman@pega.com>
2018-06-07 20:11:13 -0400

    Remove commented code

--- src/core/blocks/PublishedInputsBlock.ts
+++ src/core/blocks/PublishedInputsBlock.ts
@@ -1,11 +1,5 @@
 import { PublishableBlock } from "./PublishableBlock";
 
 export class PublishedInputsBlock extends PublishableBlock {
-	/*public getPublishedOutputValue( reference: string ): any {
-		const oldScope = this.renderingContext.getScope();
-		this.renderingContext.setScope( this.renderingContext.getParentScope() );
-		const value = this.publishedOutputs.get( reference ).getValue();
-		this.renderingContext.setScope( oldScope );
-		return value;
-	}*/
+	
 }
