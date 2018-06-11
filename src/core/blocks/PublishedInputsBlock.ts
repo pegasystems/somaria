df7d3434f8bb55c95d2a319e368e76f31af47528
Trevor Behlman <trevor.behlman@pega.com>
2018-06-10 23:49:35 -0400

    Fix PublishedInputs block

--- src/core/blocks/PublishedInputsBlock.ts
+++ src/core/blocks/PublishedInputsBlock.ts
@@ -1,5 +1,12 @@
 import { PublishableBlock } from "./PublishableBlock";
+import { RenderingContext } from "../RenderingContext";
 
 export class PublishedInputsBlock extends PublishableBlock {
-	
+	public static fromData( blockType: any, blockData: BlockJSON, renderingContext: RenderingContext ): PublishableBlock {
+		const currentScope = renderingContext.getScope();
+		renderingContext.setScope( renderingContext.getParentScope() );
+		const block = PublishableBlock.fromData( blockType, blockData, renderingContext ) as PublishedInputsBlock;
+		renderingContext.setScope( currentScope );
+		return block;
+	}
 }
